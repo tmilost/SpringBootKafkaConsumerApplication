32549c69f2c861950d899abaff8bd16cc5349e30
tmilost <46928715+tmilost@users.noreply.github.com>
2019-07-12 02:52:40 +0200

    Add files via upload

---
+++ src/main/java/kafka/spring/boot/kafka/consumer/SpringBootKafkaConsumerApplication.java
@@ -0,0 +1,12 @@
+package kafka.spring.boot.kafka.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaConsumerApplication.class, args);
+	}
+}
---
+++ src/main/java/kafka/spring/boot/kafka/consumer/listener/KafkaConsumer.java
@@ -0,0 +1,21 @@
+package kafka.spring.boot.kafka.consumer.listener;
+
+import kafka.spring.boot.kafka.consumer.model.User;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "mytopic", group = "group_id")
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+
+
+    @KafkaListener(topics = "Kafka_Example_json", group = "group_json",
+            containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user) {
+        System.out.println("Consumed JSON Message: " + user);
+    }
+}
